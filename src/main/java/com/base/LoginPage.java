811f40517b636be6704771282e8f8c049d312f40
sarunkurup3 <sarunkurup3@gmail.com>
2018-09-30 20:20:41 +0530

    add

--- src/main/java/com/base/LoginPage.java
+++ src/main/java/com/base/LoginPage.java
@@ -2,15 +2,7 @@ package com.base;
 
 public class LoginPage {
 
-	private int x =10;
-	private int y=10;
-	public void add() {
-		int sum=x+y;
-		System.out.println(sum);
-	}
-	
-	public String toString() {
-		return "the value of x is"+x+" and y is "+y;
+	public void changes() {
+		System.out.println("Checking the updates");
 	}
 };
-
